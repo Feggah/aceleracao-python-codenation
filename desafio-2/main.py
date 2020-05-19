9a2b6387cb1d347075d18fa6b2d700ea714c1fc2
Feggah <gabidferreira9@gmail.com>
2020-05-19 14:20:55 -0300

    Desafio concluido

--- desafio-2/main.py
+++ desafio-2/main.py
@@ -8,31 +8,55 @@ class Department:
 
 class Employee:
     def __init__(self, code, name, salary):
+        if type(self) == Employee:
+            raise TypeError("Classe employee não deve ser instanciada diretamente.")
         self.code = code
         self.name = name
         self.salary = salary
+        self.hours = 8
 
     def calc_bonus(self):
-        pass
+        raise NotImplementedError
 
     def get_hours(self):
-        pass
+        raise NotImplementedError
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
+        self.__departament = Department('managers', 1)
 
     def calc_bonus(self):
         return self.salary * 0.15
+
+    def get_hours(self):
+        return self.hours
+
+    def get_departament(self):
+        return self.__departament.name
+
+    def set_department(self, dep):
+        self.__departament.name = dep
 
 
 class Seller(Manager):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.sales = 0
+        self.__departament = Department('sellers', 2)
+        self.__sales = 0
 
     def get_hours(self):
-        return 6
+        return self.hours
+
+    def calc_bonus(self):
+        return self.__sales * 0.15
+
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, sales):
+        self.__sales += sales
+
+    def get_departament(self):
+        return self.__departament.name
