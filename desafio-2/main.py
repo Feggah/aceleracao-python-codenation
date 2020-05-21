5fb8a5286012b44f21edad623f042a7fb4df0e7f
Feggah <gabidferreira9@gmail.com>
2020-05-20 22:01:37 -0300

    Refatoracoes no codigo

--- desafio-2/main.py
+++ desafio-2/main.py
@@ -1,25 +1,35 @@
+from abc import ABC, abstractmethod
 
 
 class Department:
     def __init__(self, name, code):
-        self.name = name
-        self.code = code
+        self.__name = name
+        self.__code = code
+
+    # Getter e setter para os atributos de Department
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, dep):
+        self.__name = dep
 
 
-class Employee:
+# Declarando a classe Employee como abstrata
+class Employee(ABC):
+    @abstractmethod
     def __init__(self, code, name, salary):
-        if type(self) == Employee:
-            raise TypeError("Classe employee não deve ser instanciada diretamente.")
-        self.code = code
-        self.name = name
-        self.salary = salary
-        self.hours = 8
+        self.__code = code
+        self.__name = name
+        self._salary = salary
+        self._hours = 8
 
+    @abstractmethod
     def calc_bonus(self):
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def get_hours(self):
-        raise NotImplementedError
+        pass
 
 
 class Manager(Employee):
@@ -27,31 +37,31 @@ class Manager(Employee):
         super().__init__(code, name, salary)
         self.__departament = Department('managers', 1)
 
-    def calc_bonus(self):
-        return self.salary * 0.15
-
-    def get_hours(self):
-        return self.hours
-
+    # Getters e setters para os atributos de Manager
     def get_departament(self):
-        return self.__departament.name
+        return self.__departament.get_name()
 
     def set_department(self, dep):
-        self.__departament.name = dep
+        self.__departament.set_name(dep)
+
+    def get_salary(self):
+        return self._salary
+
+    # Implementação dos contratos da classe-pai
+    def get_hours(self):
+        return self._hours
+
+    def calc_bonus(self):
+        return self.get_salary() * 0.15
 
 
-class Seller(Manager):
+class Seller(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
         self.__departament = Department('sellers', 2)
         self.__sales = 0
 
-    def get_hours(self):
-        return self.hours
-
-    def calc_bonus(self):
-        return self.__sales * 0.15
-
+    # Getters e setters para os atributos da classe Seller
     def get_sales(self):
         return self.__sales
 
@@ -59,4 +69,14 @@ class Seller(Manager):
         self.__sales += sales
 
     def get_departament(self):
-        return self.__departament.name
+        return self.__departament.get_name()
+
+    def get_salary(self):
+        return self._salary
+
+    # Implementação dos contratos da classe-pai
+    def get_hours(self):
+        return self._hours
+
+    def calc_bonus(self):
+        return self.get_sales() * 0.15
